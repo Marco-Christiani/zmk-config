103b2b8b748c626c69e0d6c76b2a54f09bd1e74e
Marco Christiani <mchristiani2017@gmail.com>
2022-03-07 22:30:10 -0500

    reorg
    Signed-off-by: Marco Christiani <mchristiani2017@gmail.com>

--- viz/keymap-parser.py
+++ viz/keymap-parser.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 from pprint import pprint
 from prettytable import PrettyTable, ALL
@@ -19,124 +20,117 @@ def remove_symbols(arr: List[str]):
 			result.append(s) 
 	return result
 
+def keymap_to_dict():
+	with open("/Users/mchris/zmk-config/config/kyria.keymap", "r") as f:
+		data = f.readlines()
 
-with open("/Users/mchris/zmk-config/config/kyria.keymap", "r") as f:
-	data = f.readlines()
+	layer_names = [l.split("#define ")[1].strip().split()[0] for l in data if "#define" in l]
 
-layer_names = [l.split("#define ")[1].strip().split()[0] for l in data if "#define" in l]
+	idx = [i for i, s in enumerate(data) if "keymap {" in s][0]
 
-idx = [i for i, s in enumerate(data) if "keymap {" in s][0]
+	layers = []
+	for i in range(idx, len(data)):
+		line = data[i]
+		if "\tbindings = " in line:
+			layer = data[(i+1):(i+5)]
+			layers.append(layer)
 
-layers = []
-for i in range(idx, len(data)):
-	line = data[i]
-	if "\tbindings = " in line:
-		layer = data[(i+1):(i+5)]
-		layers.append(layer)
+	processed_keymap = {}
+	for name, layer in zip(layer_names, layers):
+		temp = []
+		for line in layer:
+			if line != "\n":
+				keys = line.split("&")
+				key_list = remove_symbols(keys)
+				temp.append(key_list)
+		processed_keymap[name] = temp
+	kc = 0
+	for layer in processed_keymap.values():
+		for row in layer:
+			kc += len(row)
 
-processed_keymap = {}
-for name, layer in zip(layer_names, layers):
-	temp = []
-	for line in layer:
-		if line != "\n":
-			keys = line.split("&")
-			key_list = remove_symbols(keys)
-			temp.append(key_list)
-	processed_keymap[name] = temp
-kc = 0
-for layer in processed_keymap.values():
-	for row in layer:
-		print(len(row))
-		kc += len(row)
+	print("Layers:", layer_names)
+	print(len(processed_keymap), "rows of keys.")
+	print(kc, "total keys (including &trans)")
 	print()
+	return processed_keymap
 
-print("Layers:", layer_names)
-print(len(processed_keymap), "rows of keys.")
-print(kc, "total keys (including &trans)")
-pprint(processed_keymap, compact=True, width=200, sort_dicts=False)
 
-def pad_rows(layer):
-	# Turn into a 4x16 grid so we can pretty print it
-	# top two layers need 2 extra keys on the inside
-	# bottom thumb tow needs three extra keys on the outside
-	result = layer.copy()
-	result[0] = layer[0][:6] + ["", ""] + ["", ""] + layer[0][6:]
-	result[1] = layer[1][:6] + ["", ""] + ["", ""] + layer[1][6:]
-	result[3] = ["", "", ""] + layer[3][:6] + layer[3][6:] + ["", "", ""]
-	return result
+def keymap_dict_to_console(processed_keymap):
+	def pad_rows(layer):
+		# Turn into a 4x16 grid so we can pretty print it
+		# top two layers need 2 extra keys on the inside
+		# bottom thumb tow needs three extra keys on the outside
+		result = layer.copy()
+		result[0] = layer[0][:6] + ["", ""] + ["", ""] + layer[0][6:]
+		result[1] = layer[1][:6] + ["", ""] + ["", ""] + layer[1][6:]
+		result[3] = ["", "", ""] + layer[3][:6] + layer[3][6:] + ["", "", ""]
+		return result
 
-# ASCII art part
-# Pad the rows so we have a neat grid
-for name in processed_keymap.keys():
-	processed_keymap[name] = pad_rows(processed_keymap[name])
-	
-#                                                  /-----------------\                                                  
-# -------------------------------------------------| Pretty Printing |--------------------------------------------------
-#                                                  \-----------------/                                                  
-# Print the table to console
-for name, layer in processed_keymap.items():
+	# ASCII art
+	# Pad the rows so we have a neat grid
+	for name in processed_keymap.keys():
+		processed_keymap[name] = pad_rows(processed_keymap[name])
+
+	# Print the table to console
+	for name, layer in processed_keymap.items():
+		table = PrettyTable(hrules=ALL, header=False)
+		table.add_rows(layer)
+		print(name)
+		print(table)
+		print()
+
+def keymap_dict_to_html(processed_keymap, dir="output/"):
+	"""
+	Build a single html file from each table's html str
+	"""
 	table = PrettyTable(hrules=ALL, header=False)
-	table.add_rows(layer)
-	print(name)
-	print(table)
-	print()
-
-# Alternatively as a single big table
-# table = PrettyTable(hrules=ALL, header=False)
-# for name, layer in processed_keymap.items():
-# 	table.add_row([name]+[""]*15)
-# 	table.add_rows(layer)
-# print(table)
-
-# html_str = table.get_html_string(format=True)
-# soup = BeautifulSoup(html_str, 'html.parser')
-# with open("keymap-viz.js", "r") as f:
-# 	script = soup.new_tag("script")
-# 	script.string = f.read()
-# 	soup.append(script)
-
-# with open("output/output.html", "w", encoding = 'utf-8') as file:    
-#     file.write(str(soup.prettify()))
-
-#                                                    /-------------\                                                    
-# ---------------------------------------------------| HTML Export |----------------------------------------------------
-#                                                    \-------------/                                                    
-# Built a single html file from each table's html str
-# First build a table for layer 0
-table = PrettyTable(hrules=ALL, header=False)
-layer_name = layer_names[0]
-table.add_rows(processed_keymap[layer_name])
-html_str = table.get_html_string(format=True)
-soup = BeautifulSoup(html_str, 'html.parser')
-h = soup.new_tag("h4")
-h.string = layer_name
-soup.table.insert_before(h)
-
-
-for layer_name in layer_names[1:]:
-	table = PrettyTable(hrules=ALL, header=False)
+	layers =  list(processed_keymap.keys())
+	layer_name = layers[0]  # First build table for layer 0
 	table.add_rows(processed_keymap[layer_name])
 	html_str = table.get_html_string(format=True)
-	curr_soup = BeautifulSoup(html_str, 'html.parser')
-	tr = curr_soup.new_tag("tr")
-	td = curr_soup.new_tag("td")
+	soup = BeautifulSoup(html_str, 'html.parser')
+
+	tr = soup.new_tag("tr")
+	td = soup.new_tag("td")
 	td.attrs["colspan"] = 16
-	h = curr_soup.new_tag("h4")
+	h = soup.new_tag("h4")
 	h.string = layer_name
 	td.append(h)
 	tr.append(td)
-	curr_soup.table.tr.insert_before(tr) # insert as first row in table
-	for tr in curr_soup.find_all("tr"):
-		soup.find_all("tr")[-1].insert_after(tr)
-	
-	
+	soup.table.tr.insert_before(tr) # insert as first row in table
 
-with open("keymap-viz.js", "r") as f:
-	script = soup.new_tag("script")
-	script.string = f.read()
-	soup.append(script)
+	# Append rows to existing html table
+	for layer_name in layers[1:]:
+		table = PrettyTable(hrules=ALL, header=False)
+		table.add_rows(processed_keymap[layer_name])
+		html_str = table.get_html_string(format=True)
+		curr_soup = BeautifulSoup(html_str, 'html.parser')
+		tr = curr_soup.new_tag("tr")
+		td = curr_soup.new_tag("td")
+		td.attrs["colspan"] = 16
+		h = curr_soup.new_tag("h4")
+		h.string = layer_name
+		td.append(h)
+		tr.append(td)
+		curr_soup.table.tr.insert_before(tr) # insert as first row in table
+		for tr in curr_soup.find_all("tr"):
+			soup.find_all("tr")[-1].insert_after(tr)
+		
+	# Inject js to highlight mod keys, layers, etc
+	with open("keymap-viz.js", "r") as f:
+		script = soup.new_tag("script")
+		script.string = f.read()
+		soup.append(script)
 
-with open("output/output.html", "w", encoding = 'utf-8') as file:    
-    file.write(str(soup.prettify()))
+	path = os.path.join(dir, "output.html")
+	with open(path, "w", encoding = 'utf-8') as file:    
+	    file.write(str(soup.prettify()))
+	    print("Saved:", path)
 
-	
+
+if __name__ == '__main__':
+	km = keymap_to_dict()
+	keymap_dict_to_console(km)
+	keymap_dict_to_html(km)
+	# pprint(processed_keymap, compact=True, width=200, sort_dicts=False)
